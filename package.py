11be20df02591abc2728a3f08c88314a0747fad0
Marcelo Sercheli <marcelo.sercheli@hernehill.com>
2024-08-22 09:41:00 -0400

    Inital files

---
+++ package.py
@@ -0,0 +1,31 @@
+name = 'qumulo_fs_walk'
+
+version = '1.0.0'
+
+authors = [
+    'Qumulo',
+]
+
+description = '''Qumulo file system walk and actions'''
+
+with scope('config') as c:
+    import os
+    c.release_packages_path = os.environ['HH_REZ_REPO_RELEASE_EXT']
+
+requires = [
+    "qumulo_api",
+]
+
+private_build_requires = [
+]
+
+variants = [
+]
+
+def commands():
+    env.REZ_QUMULO_FS_WALK_ROOT = '{root}'
+    env.PYTHONPATH.append('{root}')
+
+
+build_command = 'rez python {root}/rez_build.py'
+uuid = 'repository.qumulo-filesystem-walk'
